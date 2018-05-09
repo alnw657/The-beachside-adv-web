8879cf79e7ea2b49a1337ecd31fe044fec6030b3
Alan Wong <5755@ait.nsw.edu.au>
2018-05-09 07:05:55 +0000

    index updates and test images

--- includes/head.php
+++ includes/head.php
@@ -1,12 +1,13 @@
 <head>
   <title><?php echo $page_title; ?></title>
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <link href="../css/style.css" rel="stylesheet">
-  <link href="node_modules/bootstrap/scss/style.scss" rel="stylesheet">
-  <script src="/node_modules/jquery/dist/jquery.js"></script>
-  <script src="/node_modules/popper.js/dist/umd/popper.js"></script>
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  
   
   
   
