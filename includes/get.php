8d877e1ada894620f605852c93de4295d2ed37de
Alan Wong <5755@ait.nsw.edu.au>
2018-05-26 07:30:27 +0000

    Fernando, get.php created

---
+++ includes/get.php
@@ -0,0 +1,24 @@
+ <?php
+   
+   
+        $host = "127.0.0.1";
+            $user = "alnw657";                     
+            $pass = "";                                  
+            $db = "db";                                  
+            $port = 3306;                            
+            
+            $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+   
+        $room_id = $_GET['room_id'];
+        $roomQuery = "SELECT room_id FROM 'Rooms'";
+        
+        
+        if(isset($_GET['room_id'])){
+            echo $room_id;
+        }
+        else{
+            echo "Error";
+        }
+     
+   
+   ?>
